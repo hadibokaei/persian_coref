51c27c8ae9d39d47436342faae4c68a2ed1477cb
bokaei <m.hadibokaei@gmail.com>
2019-07-13 21:41:04 +0430

    به روزرسانی

--- model.py
+++ model.py
@@ -112,8 +112,8 @@ class CorefModel(object):
         self.candidate_phrase_probability = tf.squeeze(tf.keras.layers.Dense(1, activation='sigmoid')(dense_output)) # shape = [# of candidate phrases]
 
     def add_phrase_loss_train(self):
-        self.phrase_identification_loss = tf.reduce_mean(- tf.math.multiply(self.candidate_phrase_probability,tf.math.log(tf.to_float(self.gold_phrases))) \
-                                          - tf.math.multiply(1-self.candidate_phrase_probability,tf.math.log(1-tf.to_float(self.gold_phrases))))
+        self.phrase_identification_loss = tf.reduce_mean(- tf.math.multiply(tf.to_float(self.gold_phrases),tf.math.log(self.candidate_phrase_probability)) \
+                                          - tf.math.multiply(1-tf.to_float(self.gold_phrases),tf.math.log(1-self.candidate_phrase_probability)))
         self.phrase_identification_train = tf.train.AdamOptimizer(learning_rate=0.001).minimize(self.phrase_identification_loss)
 
     def train_phrase_identification(self, word_embedding, all_docs_word_ids, all_docs_char_ids, all_docs_phrase_indices
