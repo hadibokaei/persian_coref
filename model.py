ac8073f76f0005e2d6b44df11d5b40b6cf91335a
bokaei <m.hadibokaei@gmail.com>
2019-07-13 21:42:29 +0430

    به روزرسانی

--- model.py
+++ model.py
@@ -139,8 +139,8 @@ class CorefModel(object):
                 [_, loss, p] = self.sess.run([self.phrase_identification_train, self.phrase_identification_loss, self.candidate_phrase_probability], feed_dict)
 
                 logger.info("epoch: {} batch: {} phrase identification loss on train: {}".format(epoch, batch_number, loss))
-                # print(repr(p))
-                # print(repr(self.gold_phrases))
+                print(repr(p))
+                print(repr(self.gold_phrases))
 
 
 
