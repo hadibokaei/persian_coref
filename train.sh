7d5c124279b43180d0cc2b208bf38e4f07e26a70
bokaei <m.hadibokaei@gmail.com>
2019-07-13 16:01:48 +0430

    به روز رسانی

--- train.sh
+++ train.sh
@@ -9,7 +9,9 @@ wget -c "https://www.dropbox.com/s/3m3tzxhffvjivj6/train.zip?dl=0"
 mv "train.zip?dl=0" files/data/train.zip
 unzip files/data/train.zip -d files/data/
 
+pwd
+echo 1
 cp $1 files/we.vec
-
+echo 2
 python script_prepare_data.py
 python script_train.py
