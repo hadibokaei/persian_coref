d10a69cd354b17642323fbfaf21df6c7953053f1
bokaei <m.hadibokaei@gmail.com>
2019-07-13 18:21:22 +0430

    به روزرسانی

--- train.sh
+++ train.sh
@@ -5,13 +5,15 @@ rm -rf files
 mkdir files
 mkdir files/data
 
-wget -c "https://www.dropbox.com/s/3m3tzxhffvjivj6/train.zip?dl=0"
-mv "train.zip?dl=0" files/data/train.zip
-unzip files/data/train.zip -d files/data/
-
 pwd
 echo 1
-cp $1 "files/we.vec"
+cp $1 files/we.vec
 echo 2
+
+
+#wget -c "https://www.dropbox.com/s/3m3tzxhffvjivj6/train.zip?dl=0"
+#mv "train.zip?dl=0" files/data/train.zip
+#unzip files/data/train.zip -d files/data/
+
 #python script_prepare_data.py
 #python script_train.py
