b002122d7cba24bb85f64d1d3515c3f3ff846c17
bokaei <m.hadibokaei@gmail.com>
2019-07-14 14:05:15 +0430

    به روز رسانی

--- model.py
+++ model.py
@@ -115,7 +115,7 @@ class CorefModel(object):
         # self.phrase_identification_loss = tf.reduce_sum(- tf.math.multiply(tf.to_float(self.gold_phrases),tf.math.log(self.candidate_phrase_probability)) \
         #                                   - tf.math.multiply(1-tf.to_float(self.gold_phrases),tf.math.log(1-self.candidate_phrase_probability)))
         self.phrase_identification_loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.to_float(self.gold_phrases)
-                                                                                                , logits=self.candidate_phrase_probability)).eval()
+                                                                                                , logits=self.candidate_phrase_probability))
         self.phrase_identification_train = tf.train.AdamOptimizer(learning_rate=0.001).minimize(self.phrase_identification_loss)
 
     def train_phrase_identification(self, word_embedding, all_docs_word_ids, all_docs_char_ids, all_docs_phrase_indices
