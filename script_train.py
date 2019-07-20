271b5fdd42506b27a400110a4f216823f3aa3761
bokaei <m.hadibokaei@gmail.com>
2019-07-20 21:07:27 +0430

    add validation prf

--- script_train.py
+++ script_train.py
@@ -96,7 +96,7 @@ model.build_graph()
 
 model.train_phrase_identification(word_embedding, train_docs_word_ids, train_docs_char_ids, train_docs_phrase_indices
                                     , train_docs_gold_phrases, train_docs_phrase_length
-                                    , epoch_start=0, max_epoch_number=50)
+                                    , epoch_start=0, max_epoch_number=30)
 
 model.train_pair_identification(word_embedding
                                 , train_docs_word_ids, train_docs_char_ids, train_docs_phrase_indices
