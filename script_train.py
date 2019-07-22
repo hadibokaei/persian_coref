463a0e6c88a2cc34100c24e76131ae205c50d5ea
bokaei <m.hadibokaei@gmail.com>
2019-07-22 13:46:17 +0430

    a few changes

--- script_train.py
+++ script_train.py
@@ -43,7 +43,7 @@ model = CorefModel(word_vocab_size=vocab.last_word_index + 1, char_vocab_size=vo
 
 model.build_graph()
 
-model.train_phrase_identification(word_embedding, train_files_path[:10], train_files_path[:10], epoch_start=0, max_epoch_number=100)
+model.train_phrase_identification(word_embedding, train_files_path[:5], train_files_path[:5], epoch_start=0, max_epoch_number=500)
 
 # model.train_pair_identification(word_embedding, train_files_path, validation_files_path, epoch_start=0, max_epoch_number=100)
 
